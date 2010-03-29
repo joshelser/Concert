fc8afc2809ac6a063b3c4e358755786de4ca7f93
joshelser <josh.elser@gmail.com>
2010-03-28 20:14:53 -0400

    DON'T use tabs in python files!!! 4 spaces!!!! Also, add an attribute for tags to denote permance

--- concertapp/concert/models.py
+++ concertapp/concert/models.py
@@ -12,48 +12,49 @@ class Blogpost(models.Model):
         return self.title
 
 class User(models.Model):
-	name = models.CharField(max_length = 30, unique = True)
-	passwd = models.CharField(max_length = 20)
-	email = models.CharField(max_length = 50)
+    name = models.CharField(max_length = 30, unique = True)
+    passwd = models.CharField(max_length = 20)
+    email = models.CharField(max_length = 50)
 
 class Comments(models.Model):
-	comment = models.TextField()
-	uid = models.ForeignKey(User)
-	time = models.DateTimeField(auto_now_add = True)
-	
+    comment = models.TextField()
+    uid = models.ForeignKey(User)
+    time = models.DateTimeField(auto_now_add = True)
+    
 class Group(models.Model):
-	gname = models.CharField(max_length = 50, unique = True)
-	admin = models.ForeignKey(User)
-	
+    gname = models.CharField(max_length = 50, unique = True)
+    admin = models.ForeignKey(User)
+    
 class Audio(models.Model):
-	file = models.CharField(max_length = 100, unique = True)
-	source = models.ForeignKey(User)
-	wav = models.CharField(max_length = 100, unique = True)
+    file = models.CharField(max_length = 100, unique = True)
+    source = models.ForeignKey(User)
+    wav = models.CharField(max_length = 100, unique = True)
 
 class GroupAudio(models.Model):
-	gid = models.ForeignKey(Group)
-	cid = models.ForeignKey(Comments)
-	label = models.TextField()
-	
+    gid = models.ForeignKey(Group)
+    cid = models.ForeignKey(Comments)
+    label = models.TextField()
+    
 class AudioSegment(models.Model):
-	beginning = models.DecimalField(max_digits = 10, decimal_places = 2)
-	end = models.DecimalField(max_digits = 10, decimal_places = 2)
+    beginning = models.DecimalField(max_digits = 10, decimal_places = 2)
+    end = models.DecimalField(max_digits = 10, decimal_places = 2)
 
 class Tags(models.Model):
-	sid = models.ForeignKey(AudioSegment)
-	aid = models.ForeignKey(GroupAudio)
-	gid = models.ForeignKey(Group)
-	tag = models.CharField(max_length = 100)
-	isProject = models.BooleanField()
-	
+    sid = models.ForeignKey(AudioSegment)
+    aid = models.ForeignKey(GroupAudio)
+    gid = models.ForeignKey(Group)
+    tag = models.CharField(max_length = 100)
+    isProject = models.BooleanField()
+    isFixture = models.BooleanField()
+    
 class UserGroup(models.Model):
-	uid = models.ForeignKey(User)
-	gid = models.ForeignKey(Group)
+    uid = models.ForeignKey(User)
+    gid = models.ForeignKey(Group)
 
 class AudioGroup(models.Model):
-	aid = models.ForeignKey(Audio)
-	gid = models.ForeignKey(Group)
-	
-	
+    aid = models.ForeignKey(Audio)
+    gid = models.ForeignKey(Group)
+    
+    
 
 admin.site.register(Blogpost)
