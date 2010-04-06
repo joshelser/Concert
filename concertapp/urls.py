362c06ea98573a48d480913312fb47a66f8cdbf0
joshelser <josh.elser@gmail.com>
2010-04-05 22:38:25 -0400

    Remove the concert prefix again

--- concertapp/urls.py
+++ concertapp/urls.py
@@ -5,10 +5,10 @@ from django.conf import settings
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^concert/', include('concertapp.concert.urls')),
-    (r'^concert/api/', include('concertapp.api.urls')),
-    (r'^concert/admin/(.*)', admin.site.root),
+    (r'^', include('concertapp.concert.urls')),
+    (r'^api/', include('concertapp.api.urls')),
+    (r'^admin/(.*)', admin.site.root),
     
-    (r'^concert/accounts/login/$', 'django.contrib.auth.views.login'),
-    (r'^concert/accounts/logout/$', 'django.contrib.auth.views.logout'),
+    (r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    (r'^accounts/logout/$', 'django.contrib.auth.views.logout'),
 )
