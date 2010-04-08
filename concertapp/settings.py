475dc250ae04308d696dbdb00fd1348ba3d1e2b6
joshelser <josh.elser@gmail.com>
2010-04-08 16:17:39 -0400

    Move the database over to mysql

--- concertapp/settings.py
+++ concertapp/settings.py
@@ -16,12 +16,15 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 # example and we ship it a couple of directories up.
 sys.path.insert(0, os.path.join(BASE_DIR, '../../'))
 
-DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = os.path.join(BASE_DIR, 'db')             # Or path to database file if using sqlite3.
-#DATABASE_USER = ''             # Not used with sqlite3.
-#DATABASE_PASSWORD = ''         # Not used with sqlite3.
-#DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-#DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+#DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+#DATABASE_NAME = os.path.join(BASE_DIR, 'db')             # Or path to database file if using sqlite3.
+
+DATABASE_ENGINE = 'mysql'
+DATABASE_NAME = 'concert'
+DATABASE_USER = 'concert'             # Not used with sqlite3.
+DATABASE_PASSWORD = 'concert'         # Not used with sqlite3.
+DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
