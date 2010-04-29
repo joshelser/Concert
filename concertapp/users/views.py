19910ce69f8f2866cde312f3a9e64d9e3d718c21
joshelser <josh.elser@gmail.com>
2010-04-28 23:54:01 -0400

    Fix up the imports

--- concertapp/users/views.py
+++ concertapp/users/views.py
@@ -1,21 +1,16 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render_to_response, get_object_or_404
+from django.shortcuts import render_to_response
 from django.template import RequestContext
-from django.views.generic.create_update  import create_object
-from django.views.generic.simple import direct_to_template
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import Group, User
 from django import forms
-
+from django.contrib.auth.forms import PasswordChangeForm
 from django.http import Http404
 
 from concertapp.models  import *
 from concertapp.forms   import RegistrationForm, CreateGroupForm
-from django.contrib.auth.forms import PasswordChangeForm
-
 from concertapp.settings import MEDIA_ROOT, LOGIN_REDIRECT_URL
-
     
 def users(request):
     users = User.objects.all()
