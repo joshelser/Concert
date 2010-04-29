32b092987a6c2b3fa1baac624f39c33e21f1e822
joshelser <josh.elser@gmail.com>
2010-04-28 23:44:29 -0400

    Remove unnecessary imports

--- concertapp/groups/views.py
+++ concertapp/groups/views.py
@@ -1,13 +1,8 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render_to_response, get_object_or_404
+from django.shortcuts import render_to_response
 from django.template import RequestContext
-from django.views.generic.create_update  import create_object
-from django.views.generic.simple import direct_to_template
-from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import Group
-
-from django import forms
 
 from django.conf import settings
 
