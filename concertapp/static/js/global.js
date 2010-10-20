342648c39579497312f14c3950c5b8c9a07c60f4
Colin Sullivan <colinsul@gmail.com>
2010-10-19 20:43:44 -0400

    More organization.
    Moving functions to separate files for clarity.

--- concertapp/static/js/global.js
+++ concertapp/static/js/global.js
@@ -21,14 +21,18 @@ if(!com.concertsoundorganizer.compatibility) {
 
 $(document).ready(function(){
     
-    initializeAutoClearFieldBehavior();
+    /* This will throw a modal window to the user if there is a problem. */
+    detectBrowserCompatibility();
     
-    /**
-    *    Preload certain images
-    *
-    var images = [];
-    images.push($('<img />').attr('src', '/graphics/button_press.png'));
-    images.push($('<img />').attr('src', '/graphics/ajax-loader.gif'));
+    /* Make all fields in the future that have the "autoClear" class on them    
+        autoclear */
+    initializeAutoClearFieldBehavior();
+
+    /* image urls put here will be loaded 
+    preloadImages([
+        '/graphics/ajax-loader.gif',
+        '/graphics/somethingelse.jpg'
+    ]);
     */
 
     /**
@@ -38,43 +42,6 @@ $(document).ready(function(){
         speed: 200,
     }
 
-    /**
-     *  Detect audio compatibility
-     **/
-    if(Modernizr.audio) {
-        if(Modernizr.audio.ogg) {
-            com.concertsoundorganizer.compatibility.audiotype = 'ogg';
-        }
-        else if(Modernizer.audio.mp3) {
-            com.concertsoundorganizer.compatibility.audiotype = 'mp3';
-        }
-        else {
-            /* TODO: Handle this case */
-            alert('Site will not work.  Your browser says it doesn\'t support ogg or mp3 files, but that it supports the <audio> element.');
-        }
-    }
-    else {
-        /* TODO: Handle old browsers here */
-        alert('Site will not work.  Your browser does not support the HTML5 audio element.');
-    }
 
 
 });
-
-/**
-*  loading
-*  Global function for turning on the loading notification on the entire page.
-**/
-function loading() {
-    /* Turn on global loading notification */
-    $('#viewer_image').append('<img id="loader" src="/graphics/ajax-loader.gif" />');
-}
-
-/**
-*  loading
-*  Global function for turning off the loading notification on the entire page.
-**/
-function remove_loading() {
-    /* Remove loading notification */
-    $('#loader').remove();
-}
--- concertapp/static/js/lib/UIHelpers.js
+++ concertapp/static/js/lib/UIHelpers.js
@@ -35,3 +35,42 @@ function initializeAutoClearFieldBehavior(){
     });
     
 }
+
+/**
+ *  Preloads all images via an image beacon to the server.
+ *
+ *  @param  urls        Array 
+ **/
+function preloadImages(urls) {
+    var images = [];
+    for(i = 0, il = urls.length; i < il; i++) {
+        images.push($('<img />').attr('src', urls[i]));
+    }
+}
+
+/**
+ *  Detect browser compatibility problems, and notify user of any issues.  Should
+ *  be called on intro page, and the user should not be allowed to go further if
+ *  there are problems.
+ **/
+function detectBrowserCompatibility() {
+    /**
+     *  Detect audio compatibility, and throw notifications to the user if necessary.
+     **/
+    if(Modernizr.audio) {
+        if(Modernizr.audio.ogg) {
+            com.concertsoundorganizer.compatibility.audiotype = 'ogg';
+        }
+        else if(Modernizer.audio.mp3) {
+            com.concertsoundorganizer.compatibility.audiotype = 'mp3';
+        }
+        else {
+            /* TODO: Handle this case */
+            alert('Site will not work.  Your browser says it doesn\'t support ogg or mp3 files, but that it supports the <audio> element.');
+        }
+    }
+    else {
+        /* TODO: Handle old browsers here */
+        alert('Site will not work.  Your browser does not support the HTML5 audio element.');
+    }
+}
