f559f4d9d8e2b61f3fa6b492edbd5c9c95d74a5c
Adam Georgiou <apg552@gmail.com>
2010-10-26 14:58:53 -0400

    added the unicode function to each event class so you can print each event by just treating an event like a string

--- concertapp/dashboard/views.py
+++ concertapp/dashboard/views.py
@@ -14,10 +14,6 @@ from django.template import RequestContext
 def dashboard(request):
     user = request.user
     
-    username = user.username
-    
-    
     return render_to_response('dashboard/dashboard.html', {
-        'welcome_message': 'Welcome to Concert, '+username+'.',
-        'page_name': 'Dashboard'
+        'page_name': 'Dashboard',
     }, RequestContext(request));
