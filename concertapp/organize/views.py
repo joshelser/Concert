43dac0f80f59835b13221ca994b253dfa3a47391
Colin Sullivan <colinsul@gmail.com>
2010-11-07 00:51:40 -0400

    Added decorator file to check if a user is in col..
    lection automatically.  Reduces much code that needs to be run
    on every request.

--- concertapp/organize/views.py
+++ concertapp/organize/views.py
@@ -5,7 +5,7 @@ from django.template import RequestContext
 from django.core.cache import cache
 from django.utils import simplejson
 from django.core.files.uploadedfile import SimpleUploadedFile
-
+from django.core import serializers
 from django.core.exceptions import ObjectDoesNotExist
 
 import os, hashlib, tempfile, audiotools, tempfile
@@ -16,25 +16,22 @@ from concertapp.audio import audioFormats, audioHelpers
 from concertapp.audio.waveform import *
 from concertapp.settings import MEDIA_ROOT, LOGIN_REDIRECT_URL
 
+from concertapp.decorators import user_is_member_and_collection_exists
+
 
 ###
 #   The main organize page for a collection.  This is where we do it all.
+#
+#   @param  collection_id       Number  -  The id of the collection, populated from
+#                               the url.
+#   @param  col                 Collection  -    The collection object, passed in
+#                               from decorator, so we don't have to query for it
+#                               again.
+#   @param  user                User - the user object, passed in from decorator  
 ###
 @login_required
-def organize_collection(request, collection_id):
-    
-    user = request.user
-    
-    # Get specified collection
-    try:
-        col = Collection.objects.get(id = collection_id)
-        
-        # Make sure user is a member 
-        col.users.get(id = user.id)
-        
-    except ObjectDoesNotExist, e:
-        return HttpResponse('Error: Invalid collection.')
-        
+@user_is_member_and_collection_exists
+def organize_collection(request, collection_id, col, user):
     
     
     files = Audio.objects.filter(collection = col)
@@ -47,3 +44,41 @@ def organize_collection(request, collection_id):
         'files': files,
         'segments': segments
     }, RequestContext(request));
+    
+###
+#   This controller produces a list of audio files for the audio list panel
+###
+@login_required
+@user_is_member_and_collection_exists
+def audio_files(request, collection_id, col, user):
+    
+    fileObjects = Audio.objects.filter(collection = col)
+    
+    data = serializers.serialize('json', Audio.objects.filter(collection = col), extras=('audiosegment_set.all()',))
+    
+    return HttpResponse(data, content_type='data/json')
+    
+    """
+    filesForJSON = list()
+    
+    for f in fileObjects:
+        
+        # The uploader for this file
+        uploader = dict({
+            'name': f.uploader.username,
+            'id': f.uploader.id            
+        })
+        
+        #
+        
+        
+        #Build json result
+        filesForJSON.append(dict({
+            'id': f.id,
+            'name': f.name,
+            'uploader': uploader,
+            'segments': 
+        }));
+    """
+    
+    
