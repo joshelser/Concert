eef95d79258deca166ca267de36f129a172ab588
Adam Georgiou <apg552@gmail.com>
2010-12-01 16:37:38 -0500

    added a generic panel tag which accepts a partial template location, an id, and a title, and renders the entire thing in place of the original tag

--- concertapp/templatetags/panels.py
+++ concertapp/templatetags/panels.py
@@ -1,6 +1,42 @@
 from django import template
+from django.template.loader import render_to_string
 
 register = template.Library()
+
+
+def get_panel(parser, token):
+    try:
+        tag_name, template_location, id, title = token.split_contents()
+    except ValueError:
+        raise template.TemplateSyntaxError, "%r tag requires a two arguments" % token.contents.split()[0]
+    
+    def is_quoted(a_string):
+        if (a_string[0] == a_string[-1] and a_string[0] in ('"', "'")):
+            return True
+        return False
+
+    if not is_quoted(template_location):
+        raise template.TemplateSyntaxError, "%r tag's 1st argument should be in quotes" % tag_name
+    if not is_quoted(id): 
+        raise template.TemplateSyntaxError, "%r tag's 2nd argument should be in quotes" % tag_name
+    if not is_quoted(title):
+        raise template.TemplateSyntaxError, "%r tag's 3rd argument should be in quotes" % tag_name
+
+    return PanelNode(template_location[1:-1], id[1:-1], title[1:-1])
+
+class PanelNode(template.Node):
+    def __init__(self, template_location, id, title):
+        self.template_location = template_location
+        self.id = id
+        self.title = title
+    def render(self, context):
+        return render_to_string(self.template_location, {'id': self.id, 'title':self.title}, context)
+
+register.tag('panel2', get_panel)
+
+
+
+
 
 ###
 #   This is a partial template for a panel.  All panels will have this same stuff.
