745e5efae4331f37ff814d691811706245915b7f
Colin Sullivan <colinsul@gmail.com>
2011-01-18 12:06:26 -0500

    Fixed minor issue so master branch works again

--- concertapp/dashboard/urls.py
+++ concertapp/dashboard/urls.py
@@ -3,8 +3,6 @@ from django.conf.urls.defaults import *
 from django.contrib import admin
 from django.conf import settings
 
-admin.autodiscover()
-
 urlpatterns = patterns('concertapp.dashboard.views',
     #index, just goes to dashboard view
     url(r'^$', 'dashboard', name='dashboard'),
