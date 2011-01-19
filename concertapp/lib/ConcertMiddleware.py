bf873f4adc2dcaa12fad967d3623229318d7fd96
Colin Sullivan <colinsul@gmail.com>
2011-01-18 20:51:08 -0500

    Comments.

--- concertapp/lib/ConcertMiddleware.py
+++ concertapp/lib/ConcertMiddleware.py
@@ -12,9 +12,12 @@ class ConcertBootstrapDataMiddleware(object):
     def process_template_response(self, request, response):
         user = request.user
         
+        # If the view did not create a 'data' object in the context, create
+        # an empty one.
         if 'data' not in response.context_data:
             response.context_data['data'] = {}
 
+        # If the user is logged in, we will need the following data on the page.
         if user.is_authenticated():                
             # Data for user object
             userResource = UserResource()
@@ -26,7 +29,8 @@ class ConcertBootstrapDataMiddleware(object):
             r.set_user(user)
             response.context_data['data']['memberCollectionsData'] = r.as_dict(request)
             
-        
+        # combine above data, and any data that the view sent in into a single json 
+        # object.
         response.context_data['data'] = simplejson.dumps(response.context_data['data'])
 
         return response
