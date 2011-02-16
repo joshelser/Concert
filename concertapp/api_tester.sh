37c1c6bf353ff21499d56c728db97a8739107723
Adam Georgiou <apg552@gmail.com>
2011-02-15 22:54:07 -0500

    added ability specify request type

--- concertapp/api_tester.sh
+++ concertapp/api_tester.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-
 url_base="http://localhost:8000";
-api_url="/api/1/audio/1/";
-session_id="7624df7731765ea2318628cf756c4e14";
-json_data='{"name":"new_audio","uploader":"/api/1/user/1/","collection":"/api/1/collection/1/"}'
+api_url="/api/1/audiosegment/1/"
+session_id="ba4b33ed6e45384ce97a5436191342a5";
+json_data='{"name":"asdasd","creator":"/api/1/user/1/","beginning":"1","end":"5","audio":"/api/1/audio/1/"}'
 
-curl "$url_base$api_url" -H "Content-Type:application/json" -X 'PUT' --data-binary $json_data -b "sessionid=$session_id";
+curl "$url_base$api_url" -H "Content-Type:application/json" -X 'PUT' --data-binary $json_data -b "sessionid=$session_id" > temp.html;
+
+chromium-browser temp.html;
