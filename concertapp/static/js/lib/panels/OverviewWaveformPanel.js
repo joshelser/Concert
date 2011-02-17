c691f77fed002a2be55b56e16cf6d4241096e5ae
Colin Sullivan <colinsul@gmail.com>
2011-02-17 10:39:18 -0500

    Fixed up organize page and such

---
+++ concertapp/static/js/lib/panels/OverviewWaveformPanel.js
@@ -0,0 +1,17 @@
+/**
+ *  @file       OverviewWaveformPanel.js
+ *  @author     Colin Sullivan <colinsul [at] gmail.com>
+ **/
+
+
+/**
+ *  This is the smaller waveform panel, which will be at the top underneath the 
+ *  options bar on the "organization" page.
+ *	@class
+ *  @extends    Panel
+ **/
+var OverviewWaveformPanel = Panel.extend({
+    initialize: function() {
+        
+    }, 
+})
--- concertapp/static/js/lib/widgets/CollectionInfoWidget.js
+++ concertapp/static/js/lib/widgets/CollectionInfoWidget.js
@@ -7,16 +7,10 @@
 /**
  *  This is a widget that shows the information about a collection.
  *	@class
+ *  @extends    Widget
  **/
-function CollectionInfoWidget(params) {
-    if(params) {
-        this.init(params);
-    }
-}
-CollectionInfoWidget.prototype = new Widget();
-
-CollectionInfoWidget.prototype.init = function(params) {
-    Widget.prototype.init.call(this, params);
-
-    this.render();
-}
+var CollectionInfoWidget = Widget.extend({
+    initialize: function() {
+        
+    }, 
+});
