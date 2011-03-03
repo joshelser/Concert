297697b28e24a4cac7cb2eefa715ad932bf08854
Colin Sullivan <colinsul@gmail.com>
2011-03-03 16:39:11 -0500

    Fixed some crazy indentation,.

--- concertapp/urls.py
+++ concertapp/urls.py
@@ -76,17 +76,19 @@ urlpatterns = patterns('',
 
 if settings.DEBUG:
     urlpatterns += patterns('',
-                            (r'^media/(?P<path>.*)$', 'django.views.static.serve', 
-                             {'document_root': settings.MEDIA_ROOT}),
-                            (r'^js/(?P<path>.*)$', 'django.views.static.serve', 
-                             {'document_root' : os.path.join(settings.STATIC_DOC_ROOT, 'js')}),
-                            (r'^css/(?P<path>.*)$', 'django.views.static.serve', 
-                             {'document_root' : os.path.join(settings.STATIC_DOC_ROOT, 'css')}),
-                            (r'^graphics/(?P<path>.*)$', 'django.views.static.serve', 
-                             {'document_root' : os.path.join(settings.STATIC_DOC_ROOT, 
-                                                             'graphics')}),
-                            (r'^paths/(?P<path>.*)$', 'django.views.static.serve', 
-                             {'document_root' : os.path.join(settings.STATIC_DOC_ROOT, 
-                                                             'paths')})
-                            
-                            )
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT
+        }),
+        (r'^js/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root' : os.path.join(settings.STATIC_DOC_ROOT, 'js')
+        }),
+        (r'^css/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root' : os.path.join(settings.STATIC_DOC_ROOT, 'css')
+        }),
+        (r'^graphics/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root' : os.path.join(settings.STATIC_DOC_ROOT, 'graphics')
+        }),
+        (r'^paths/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root' : os.path.join(settings.STATIC_DOC_ROOT, 'paths')
+        }),
+    )
